f0daed20cbd652654fe7f114cfa229c22949ca9c
drdamour <drdamour at google mail>
2013-04-18 15:27:26 -0600

    initial commit

---
+++ Model.Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			using (var db = new ThingsContext())
+			{
+
+				var countBefore = db.Things.Count();
+
+				var newitem = new Thingy() { Name = Guid.NewGuid().ToString() };
+				db.Things.Add(newitem);
+				db.SaveChanges();
+
+
+				var countAfter = db.Things.Count();
+
+				Assert.AreEqual(countBefore + 1, countAfter);
+
+			}
+		}
+	}
+}
---
+++ WebApp/Controllers/ThingyController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace WebApp.Controllers
+{
+    public class ThingyController : Controller
+    {
+        private ThingsContext db = new ThingsContext();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.Things.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(long id = 0)
+        {
+            Thingy thingy = db.Things.Find(id);
+            if (thingy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thingy);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Thingy thingy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Things.Add(thingy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(thingy);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(long id = 0)
+        {
+            Thingy thingy = db.Things.Find(id);
+            if (thingy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thingy);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Thingy thingy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thingy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thingy);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(long id = 0)
+        {
+            Thingy thingy = db.Things.Find(id);
+            if (thingy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thingy);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Thingy thingy = db.Things.Find(id);
+            db.Things.Remove(thingy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
